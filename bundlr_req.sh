28c7e3ec05ed3ef02eecb4eef56c76a09e94fb4f
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2022-10-09 15:32:50 +0300

    Create bundlr_req.sh

---
+++ bundlr_req.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+curl https://sh.rustup.rs -sSf | sh
+source ~/.cargo/env
+sudo apt update
+sudo apt install nodejs -y
+sudo apt install npm -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.bashrc
+nvm install 16
+nvm use 16
+sudo apt-get install curl wget git jq libpq-dev libssl-dev build-essential pkg-config openssl ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
